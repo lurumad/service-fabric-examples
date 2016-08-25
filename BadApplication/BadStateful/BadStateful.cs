0ceb7becd51bdf94ad11b9b890b1d72cd3e75e14
Lurumad <luis_ruiz_pavon@hotmail.com>
2016-08-25 14:41:16 +0200

    Add more examples

---
+++ BadApplication/BadStateful/BadStateful.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Data.Collections;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Runtime;
+
+namespace BadStateful
+{
+    /// <summary>
+    /// An instance of this class is created for each service replica by the Service Fabric runtime.
+    /// </summary>
+    internal sealed class BadStateful : StatefulService
+    {
+        public BadStateful(StatefulServiceContext context)
+            : base(context)
+        { }
+
+        /// <summary>
+        /// Optional override to create listeners (e.g., HTTP, Service Remoting, WCF, etc.) for this service replica to handle client or user requests.
+        /// </summary>
+        /// <remarks>
+        /// For more information on service communication, see http://aka.ms/servicefabricservicecommunication
+        /// </remarks>
+        /// <returns>A collection of listeners.</returns>
+        protected override IEnumerable<ServiceReplicaListener> CreateServiceReplicaListeners()
+        {
+            return new ServiceReplicaListener[0];
+        }
+
+        /// <summary>
+        /// This is the main entry point for your service replica.
+        /// This method executes when this replica of your service becomes primary and has write status.
+        /// </summary>
+        /// <param name="cancellationToken">Canceled when Service Fabric needs to shut down this service replica.</param>
+        protected override async Task RunAsync(CancellationToken cancellationToken)
+        {
+            long localValue = 0;
+            const string key = "Counter-Unique";
+
+            var myDictionary = await StateManager.GetOrAddAsync<IReliableDictionary<string, long>>("myDictionary");
+
+            while (true)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+
+                using (var tx = StateManager.CreateTransaction())
+                {
+                    var result = await myDictionary.TryGetValueAsync(tx, key);
+
+                    if (result.HasValue && localValue != result.Value)
+                    {
+                        throw new ApplicationException("State is inconsistent");
+                    }
+
+                    await myDictionary.AddOrUpdateAsync(tx, key, 1, (k, v) => ++v);
+                    localValue++;
+
+                    ServiceEventSource.Current.Message($"localValue {localValue} result.Value {result.Value}");
+
+                    await tx.CommitAsync();
+                }
+
+                await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
+            }
+        }
+    }
+}
---
+++ ChaosTests/Testability/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.Fabric.Testability.Scenario;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Testability
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var clusterConnection = "localhost:19000";
+            Console.WriteLine("Starting Chaos Test Scenario...");
+
+            try
+            {
+                RunChaosTestScenarioAsync(clusterConnection).Wait();
+            }
+            catch (AggregateException ae)
+            {
+                Console.WriteLine("Chaos Test Scenario did not complete: ");
+
+                foreach (Exception ex in ae.InnerExceptions)
+                {
+                    if (ex is FabricException)
+                    {
+                        Console.WriteLine("HResult: {0} Message: {1}", ex.HResult, ex.Message);
+                    }
+                }
+            }
+
+            Console.WriteLine("Chaos Test Scenario completed.");
+
+            Console.ReadKey();
+        }
+
+        static async Task RunChaosTestScenarioAsync(string clusterConnection)
+        {
+            var maxClusterStabilizationTimeout = TimeSpan.FromSeconds(180);
+            uint maxConcurrentFaults = 3;
+            var enableMoveReplicaFaults = true;
+
+            var fabricClient = new FabricClient(clusterConnection);
+
+            var timeToRun=  TimeSpan.FromMinutes(2);
+
+            var scenarioParameters = new ChaosTestScenarioParameters(
+                maxClusterStabilizationTimeout,
+                maxConcurrentFaults,
+                enableMoveReplicaFaults,
+                timeToRun);
+
+            var chaosTestScenario = new ChaosTestScenario(
+                fabricClient,
+                scenarioParameters);
+
+            try
+            {
+                await chaosTestScenario.ExecuteAsync(CancellationToken.None);
+            }
+            catch (Exception e)
+            {
+                throw e.InnerException;
+            }
+        }
+    }
+}
